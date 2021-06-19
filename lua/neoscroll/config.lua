1de7d035c6bbcf367d96954a02b248b4fedc5978
Carles Rafols <41967813+karb94@users.noreply.github.com>
2021-06-19 14:22:55 +0100

    Reapply "A few fixes in edge cases" and add option to use local scrolloff
    This reverts commit 9c56770db06895084610ba51fee39045eefc1567.

--- lua/neoscroll/config.lua
+++ lua/neoscroll/config.lua
@@ -5,10 +5,12 @@ config.options = {
         'zt', 'zz', 'zb'},
     hide_cursor = true,
     stop_eof = true,
+    use_local_scrolloff = false,
     respect_scrolloff = false,
     cursor_scrolls_alone = true,
     easing_function = nil
 }
+
 
 -- Default options
 function config.set_options(opts)
